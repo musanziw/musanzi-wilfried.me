d6e2c64d5f6f647e31a0711ecb633cb36c3eb4b8
Wilfried Musanzi <musanziw.m@gmail.com>
2023-10-02 19:20:55 +0200

    add: reduce gap.

--- app/components/Faq.tsx
+++ app/components/Faq.tsx
@@ -5,7 +5,7 @@ export default function Faq() {
     return (
         <div className={'p-8 md:p-0 mb-10 md:w-2/3 lg:w-1/3'}>
             <Heading label={"Frequently Asked questions"} sublabel={"FaQ"}/>
-            <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-4">
                 <QA question={"What is your availability ?"}
                     answer={"I am available for full-time work, I am also open to part-time or contract work."}
                 />
