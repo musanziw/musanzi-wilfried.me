5a32aee72204a6e94c52758f4c540ee16fdeaf1e
Wilfried Musanzi <musanziw.m@gmail.com>
2023-10-02 18:18:51 +0200

    update: change font from outfit to DM Sans.

--- app/utils/ProjectCard.tsx
+++ app/utils/ProjectCard.tsx
@@ -10,19 +10,18 @@ export default function ProjectCard({title, resume, goals, tools, position}: Pro
     return (
         <div
             className="flex flex-col gap-2">
-            <h4 className={"font-semibold font-outfit text-zinc-950 dark:text-white"}>
+            <h4 className={"font-semibold text-zinc-950 dark:text-white"}>
                 {title}
             </h4>
             <p className={"text-sm leading-5 mb-2"}>
                 {resume}
             </p>
             {
-
                 <>
-                    <h5 className={"font-semibold font-outfit text-zinc-950 dark:text-white"}>
-                        Position held : <span className={'font-medium font-inter text-sm'}>{position}</span>
+                    <h5 className={"font-semibold text-zinc-950 dark:text-white"}>
+                        Position held : <span className={'font-medium text-sm'}>{position}</span>
                     </h5>
-                    <h5 className={"font-semibold font-outfit text-zinc-950 dark:text-white mt-3"}>
+                    <h5 className={"font-semibold text-zinc-950 dark:text-white mt-3"}>
                         Goals
                     </h5>
                     <ul className="flex flex-col gap-1">
@@ -39,16 +38,15 @@ export default function ProjectCard({title, resume, goals, tools, position}: Pro
                             </li>
                         ))}
                     </ul>
-                    <h5 className={"font-semibold font-outfit text-zinc-950 dark:text-white mt-3"}>
+                    <h5 className={"font-semibold text-zinc-950 dark:text-white mt-3"}>
                         Tools
                     </h5>
                     <div className="flex flex-wrap gap-2">
                         {tools.map((tool, index) => (
-                            <span
-                                key={index}
-                                className="px-2 py-1 rounded-full bg-neutral-400/10 dark:text-white dark:bg-zinc-500/10 text-xs leading-5 font-medium font-outfit">
+                            <span key={index}
+                                  className="px-2 py-1 rounded-full bg-neutral-400/10 dark:text-white dark:bg-zinc-500/10 text-xs leading-5 font-medium">
                                     {tool}
-                                    </span>
+                            </span>
                         ))}
                     </div>
                 </>
