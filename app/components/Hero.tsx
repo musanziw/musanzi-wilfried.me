c0c3f9d5f942e30333b75ab3ad6dfe37790dc235
Wilfried Musanzi <musanziw.m@gmail.com>
2023-10-04 18:21:55 +0200

    update: update the logo and the logo font.

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,15 +1,21 @@
-import React from "react";
 import me from '@/app/images/me.webp'
 import Image from "next/image";
 import Link from "next/link";
 import ThemeSwitcher from "@/app/utils/ThemeSwitcher";
+import {Oswald} from 'next/font/google'
+
+
+const oswald = Oswald({subsets: ['latin']})
+
 
 export default function Hero() {
     return (
         <div className="px-8 mb-16 pt-6 md:pl-0 flex flex-col items-center justify-center w-full md:w-2/3 lg:w-1/3">
             <div
                 className={`flex items-center justify-between mb-20 w-full`}>
-                <h1 className={'uppercase font-bold text-xl dark:text-white text-zinc-950'}>m.</h1>
+                <h1 className={`relative ${oswald.className} uppercase text-xs font-bold dark:text-white text-zinc-950 tracking-wider before:inline-block before:absolute before:top-0 before:bg-red-400 before:h-full before:w-[2px] before:-ml-1`}>
+                    Wilfried <br/> Musanzi
+                </h1>
                 <div className="flex items-center gap-5">
                     <Link href={"https://github.com/musanziw"} target={'_blank'} aria-label={'Github link'}>
                         <svg xmlns="http://www.w3.org/2000/svg"
@@ -18,7 +24,7 @@ export default function Hero() {
                                 d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
                         </svg>
                     </Link>
-                    <ThemeSwitcher />
+                    <ThemeSwitcher/>
                 </div>
             </div>
 
@@ -28,10 +34,10 @@ export default function Hero() {
                         <Image src={me} className={"rounded-sm grayscale"} alt="Musazi's picture"/>
                     </div>
                     <div className="">
-                        <h1 className={"text-xl font-extrabold text-zinc-950 dark:text-white mb-1"}>
+                        <h1 className={`text-xl font-extrabold text-zinc-950 dark:text-white mb-1`}>
                             Hey, I&lsquo;m Wilfried Musanzi
                         </h1>
-                        <h1 className={"text-lg font-semibold  text-zinc-950 dark:text-white"}>
+                        <h1 className={`font-semibold uppercase text-xs text-stone-600 dark:text-neutral-200`}>
                             Fullsatck JavaScript developer
                         </h1>
                     </div>
