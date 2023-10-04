a2986d67bffd4234c378fd8ccd586f39c47a036b
Wilfried Musanzi <musanziw.m@gmail.com>
2023-10-04 18:20:41 +0200

    update: update the subtitle.

--- app/components/Faq.tsx
+++ app/components/Faq.tsx
@@ -4,8 +4,8 @@ import QA from "../utils/QA";
 export default function Faq() {
     return (
         <div className={'p-8 md:p-0 mb-10 md:w-2/3 lg:w-1/3'}>
-            <Heading label={"Frequently Asked questions"} sublabel={"Q/A"}/>
-            <div className="flex flex-col gap-4">
+            <Heading label={"Frequently Asked questions"} sublabel={"Questions/answers"}/>
+            <div className="flex flex-col gap-5">
                 <QA question={"What is your availability ?"}
                     answer={"I am available for full-time work, I am also open to part-time or contract work."}
                 />
