adeeb6d844b330648a1171fecbbca2d5c8f44757
Wilfried Musanzi <musanziw.m@gmail.com>
2023-10-06 14:34:09 +0200

    delete: some details in used tools.

--- app/components/Project.tsx
+++ app/components/Project.tsx
@@ -9,17 +9,17 @@ export default function Project() {
                 <ProjectCard
                     title={"Agricultural Workforce Management System"}
                     resume={"A web-based application designed exclusively for farm operations, offering comprehensive features to efficiently manage daily laborers and tasks. Additionally, it seamlessly generates detailed expense reports categorized by tasks on a weekly basis, providing farm owners with valuable insights for informed decision-making."}
-                    tools={["PHP/Laravel", "UML", "Javascript", "SCSS", "MySQL", "Git & Github", "Ubuntu", "Mutual server"]}
+                    tools={["PHP/Laravel", "UML", "Javascript", "SCSS", "MySQL"]}
                 />
 
                 <ProjectCard title={"Client Relationship Management Platform"}
                              resume={"An web application tailored specifically for startups, providing an all-in-one solution for effectively managing client contacts and establishing organized groups. This professional-grade platform enables seamless communication through SMS or Email, ensuring streamlined and personalized interactions with clients."}
-                             tools={["PHP/Laravel", "UML", "Javascript", "SCSS", "MariaDB", "Git & Github", "Fedora", "Mutual server"]}
+                             tools={["PHP/Laravel", "UML", "Javascript", "SCSS", "MariaDB"]}
                 />
 
                 <ProjectCard title={"Ecclesiastical Management Solution"}
                              resume={"A web-based application developed to meet the unique needs of churches, facilitating seamless access to events for members while empowering pastors with advanced tools for member management, event coordination, and comprehensive church reporting. This professional-grade solution ensures efficient administration, fostering a thriving church community."}
-                             tools={["ReactJS", "Node/NestJS", "API Rest", "MariaDB", "TailwindCSS", "Git & Github", "Arch Linux", "Dedicaded server"]}
+                             tools={["NextJS", "NestJS", "API Rest", "MariaDB", "TailwindCSS"]}
                 />
             </div>
         </div>
