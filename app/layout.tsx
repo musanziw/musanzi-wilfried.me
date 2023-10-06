f86513518b1fdbc133739bf73683141e888e781f
Wilfried Musanzi <musanziw.m@gmail.com>
2023-10-06 14:15:19 +0200

    refactor: cleanup code.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import './globals.css'
-import type { Metadata } from 'next'
-import { DM_Sans } from 'next/font/google'
+import type {Metadata} from 'next'
+import {DM_Sans} from 'next/font/google'
 import React from "react";
 
 
-const dmSans = DM_Sans({ subsets: ['latin'] })
+const dmSans = DM_Sans({
+    subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
     title: 'Wilfried Musanzi — Portfolio',
@@ -12,13 +14,13 @@ export const metadata: Metadata = {
     keywords: 'Wilfried Musanzi',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body
-                className={`bg-neutral-50 text-zinc-900 ${dmSans.className} dark:bg-zinc-950 dark:text-neutral-100 transition-colors ease-linear duration-100`}>
-                {children}
-            </body>
+        <body
+            className={`bg-neutral-50 text-zinc-900 ${dmSans.className} dark:bg-zinc-950 dark:text-neutral-100 transition-colors ease-linear duration-100`}>
+        {children}
+        </body>
         </html>
     )
 }
