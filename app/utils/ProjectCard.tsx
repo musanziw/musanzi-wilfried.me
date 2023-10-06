e6f092ae73d607484ed5e0b949767ec28712ce41
Wilfried Musanzi <musanziw.m@gmail.com>
2023-10-06 14:20:15 +0200

    delete: remove tools title.

--- app/utils/ProjectCard.tsx
+++ app/utils/ProjectCard.tsx
@@ -15,19 +15,14 @@ export default function ProjectCard({title, resume, tools}: ProjectCardProps) {
                 {resume}
             </p>
             {
-                <>
-                    <p className={"font-semibold text-zinc-950 dark:text-white mt-3"}>
-                        Tools
-                    </p>
-                    <div className="flex flex-wrap gap-2">
-                        {tools.map((tool, index) => (
-                            <span key={index}
-                                  className="px-2 py-1 rounded-full bg-neutral-400/10 dark:text-white dark:bg-zinc-400/10 text-xs leading-5 font-medium">
+                <div className="flex flex-wrap gap-2">
+                    {tools.map((tool, index) => (
+                        <span key={index}
+                              className="px-2 py-1 rounded-full bg-neutral-400/10 dark:text-white dark:bg-zinc-400/10 text-xs leading-5 font-medium">
                                     {tool}
                             </span>
-                        ))}
-                    </div>
-                </>
+                    ))}
+                </div>
             }
         </div>
     )
