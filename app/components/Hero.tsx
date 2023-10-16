9a707c48d28dfe72f08cd9d9682837199303c4f7
Wilfried Musanzi <musanziw.m@gmail.com>
2023-10-17 00:28:28 +0200

    update: update style

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -24,10 +24,9 @@ export default function Hero() {
                                 </div>
                                 <a href={'https://github.com/musanziw'} target={'_blank'} className="flex flex-col ml-2">
                                     <span className="text-base font-bold text-zinc-950 dark:text-white">
-                                        About me</span>
-                                    <span
-                                        className="text-sm text-neutral-700 dark:text-gray-300">
-                                        Self thoughts
+                                        Github/musanziw</span>
+                                    <span className="text-sm dark:text-gray-300">
+                                        github.com/musanziw
                                     </span>
                                 </a>
                             </div>
